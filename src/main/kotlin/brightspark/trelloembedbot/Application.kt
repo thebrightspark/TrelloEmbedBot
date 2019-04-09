ad8795f44895212845c5b129f4eb68bea0e7c0ac
bright_spark <markpassey@btinternet.com>
2019-04-09 17:11:57 +0100

    Removed properties file, moved request handling to separate class

--- src/main/kotlin/brightspark/trelloembedbot/Application.kt
+++ src/main/kotlin/brightspark/trelloembedbot/Application.kt
@@ -6,13 +6,11 @@ import net.dv8tion.jda.core.OnlineStatus
 import net.dv8tion.jda.core.entities.Game
 import net.dv8tion.jda.core.hooks.AnnotatedEventManager
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
-import java.io.File
-import java.io.FileInputStream
-import java.io.FileOutputStream
-import java.util.*
 import javax.security.auth.login.LoginException
 import kotlin.system.exitProcess
 
@@ -20,48 +18,20 @@ import kotlin.system.exitProcess
 class Application {
 	private val log = LoggerFactory.getLogger(this::class.java)
 
+	@Value("\${token}")
+	private lateinit var token: String
+
+	@Autowired
+	private lateinit var listener: Listener
+
 	@Bean
 	fun init() {
-		// Read configs
-		val configFile = File("config.properties")
-		if (!configFile.exists()) {
-			log.error("No config file! Generating default for user...")
-			val properties = Properties()
-			properties.setProperty("token", "")
-			properties.setProperty("trelloToken", "")
-			val output = FileOutputStream(configFile)
-			output.use { properties.store(it, null) }
-			exitProcess(0)
-		}
-
-		val properties = Properties()
-		val input = FileInputStream(configFile)
-		input.use { properties.load(it) }
-
-		if (!properties.containsKey("token")) {
-			log.error("No token specified in config file!")
-			exitProcess(0)
-		}
-
-		if (!properties.containsKey("trelloToken")) {
-			log.error("No trelloToken specified in config file!")
-			exitProcess(0)
-		}
-
-		val token = properties.getProperty("token")
-		Listener.trelloToken = properties.getProperty("trelloToken")
-
-		/*if (trelloToken.isBlank()) {
-			log.error("Trello API Key '$trelloToken' is invalid!")
-			exitProcess(0)
-		}*/
-
 		val builder = JDABuilder(AccountType.BOT)
 			.setToken(token)
 			.setStatus(OnlineStatus.DO_NOT_DISTURB)
 			.setGame(Game.playing("Starting up..."))
 			.setEventManager(AnnotatedEventManager())
-			.addEventListener(Listener())
+			.addEventListener(listener)
 		try {
 			builder.build()
 		}
---
+++ src/main/kotlin/brightspark/trelloembedbot/RequestHandler.kt
@@ -0,0 +1,56 @@
+package brightspark.trelloembedbot
+
+import com.fasterxml.jackson.core.JsonParseException
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import org.springframework.web.client.RestTemplate
+import org.springframework.web.client.getForEntity
+import java.io.IOException
+
+@Component
+class RequestHandler {
+    companion object {
+        const val trelloKey: String = "10a61e7cd59f0840e292b285a9b21dab"
+        val rest: RestTemplate = RestTemplate()
+        val jsonMapper: ObjectMapper = ObjectMapper()
+    }
+
+    @Value("\${trelloToken}")
+    private lateinit var trelloToken: String
+
+    @Autowired
+    private lateinit var requestLimiter: RequestLimiter
+
+    private val log = LoggerFactory.getLogger(this::class.java)
+
+    private fun get(request: String) : JsonNode? {
+        val waitTime = requestLimiter.acquire()
+        if (waitTime > 0) {
+            log.warn("Waiting for ${waitTime}ms before sending next request")
+            Thread.sleep(waitTime)
+        }
+        val response = rest.getForEntity<String>(request)
+        val json: JsonNode
+        try {
+            json = jsonMapper.readTree(response.body)
+        } catch (e: JsonParseException) {
+            log.error("Exception parsing JSON from URL: $request", e)
+            return null
+        } catch (e: IOException) {
+            log.error("Exception getting from URL: $request", e)
+            return null
+        }
+        if (response.statusCode.isError) {
+            log.error("Error getting from URL: $request\nCode: ${response.statusCodeValue}\nError: ${json.get("error")}\nMessage: ${json.get("message")}")
+            return null
+        }
+        return json
+    }
+
+    fun getCardInfo(cardId: String) : JsonNode? =
+            get("https://api.trello.com/1/cards/$cardId?key=$trelloKey&token=$trelloToken&fields=closed,dateLastActivity,desc,due,dueComplete,name&members=true&member_fields=username&checklists=all&checklist_fields=name,pos&board=true&board_fields=name&list=true&list_fields=name&labels=all&label_fields=name,color")
+}
