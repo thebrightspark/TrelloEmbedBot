b5d50b091de564e47fa5a805c7134e6f8a0e6beb
bright_spark <markpassey@btinternet.com>
2019-04-10 14:11:02 +0100

    Improved request error handling a bit

--- src/main/kotlin/brightspark/trelloembedbot/RequestHandler.kt
+++ src/main/kotlin/brightspark/trelloembedbot/RequestHandler.kt
@@ -1,18 +1,20 @@
 package brightspark.trelloembedbot
 
-import com.fasterxml.jackson.core.JsonParseException
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.InitializingBean
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.client.ClientHttpResponse
 import org.springframework.stereotype.Component
+import org.springframework.web.client.ResponseErrorHandler
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.getForEntity
 import java.io.IOException
 
 @Component
-class RequestHandler {
+class RequestHandler : InitializingBean {
     companion object {
         const val trelloKey: String = "10a61e7cd59f0840e292b285a9b21dab"
         val rest: RestTemplate = RestTemplate()
@@ -27,6 +29,14 @@ class RequestHandler {
 
     private val log = LoggerFactory.getLogger(this::class.java)
 
+    override fun afterPropertiesSet() {
+        // Don't want the error handler to do anything - will handle it later with the rest of the response handling
+        rest.errorHandler = object : ResponseErrorHandler {
+            override fun hasError(response: ClientHttpResponse): Boolean = false
+            override fun handleError(response: ClientHttpResponse) = Unit
+        }
+    }
+
     private fun get(request: String) : JsonNode? {
         val waitTime = requestLimiter.acquire()
         if (waitTime > 0) {
@@ -35,25 +45,23 @@ class RequestHandler {
         }
         log.info("HTTP Get: $request")
         val response = rest.getForEntity<String>(request)
-        log.info("Response -> Status: ${response.statusCode}")
-        log.info("Response -> Body: ${response.body}")
+        val status = response.statusCode
         val json: JsonNode
         try {
             json = jsonMapper.readTree(response.body)
-        } catch (e: JsonParseException) {
+        } catch (e: IOException) {
             log.error("Exception parsing JSON from URL: $request", e)
             return null
-        } catch (e: IOException) {
-            log.error("Exception getting from URL: $request", e)
-            return null
         }
-        if (response.statusCode.isError) {
-            log.error("Error getting from URL: $request\nCode: ${response.statusCodeValue}\nError: ${json.get("error")}\nMessage: ${json.get("message")}")
+        if (status.isError) {
+            log.error("Error getting from URL: $request\nCode: ${status.value()}\nError: ${json.get("error")}\nMessage: ${json.get("message")}")
             return null
         }
         return json
     }
 
-    fun getCardInfo(cardId: String) : JsonNode? =
-            get("https://api.trello.com/1/cards/$cardId?key=$trelloKey&token=$trelloToken&fields=closed,desc,due,dueComplete,name,labels&members=true&member_fields=username&checklists=all&checklist_fields=name,pos&board=true&board_fields=name&list=true&list_fields=name&label_fields=name,color")
+    fun getCardInfo(cardId: String) : JsonNode? {
+        log.info("Getting info for card ID '$cardId' from Trello")
+        return get("https://api.trello.com/1/cards/$cardId?key=$trelloKey&token=$trelloToken&fields=closed,desc,due,dueComplete,name,labels&members=true&member_fields=username&checklists=all&checklist_fields=name,pos&board=true&board_fields=name&list=true&list_fields=name&label_fields=name,color")
+    }
 }
